00902e6f871c8df27d46653672c3dbd8c6f90e88
stefan <dev@2904.cc>
2016-06-23 03:08:51 +0200

    disabled debuge mode for graphite logger

--- graphiteLogger/logger.py
+++ graphiteLogger/logger.py
@@ -12,7 +12,7 @@ def logSensor(logtime, sock, space, sensorname, value):
     name = 'sensors.' + space + '.' + sensorname
     data = '%s %s %s' % (name, value, logtime)
     print data
-    # sock.send(data)
+    sock.send(data)
 
 
 def logEndpoint(logtime, sock, url):
@@ -30,7 +30,7 @@ def init():
     logtime = int(now - (now % (15 * 60)))
     # print datetime.datetime.fromtimestamp(logtime).strftime('%H:%M:%S')
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # sock.connect((GRAPHITEHOST, GRAPHITEPORT))
+    sock.connect((GRAPHITEHOST, GRAPHITEPORT))
     return logtime, sock
 
 if __name__ == '__main__':
